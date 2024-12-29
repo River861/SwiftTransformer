9cc4a5553949c13d0f0eedc936aa46386623d115
interestingLSY <interestingLSY@gmail.com>
2024-12-29 11:05:44 +0800

    Upgrade countNanKernel

--- src/csrc/kernel/count_nan.cu
+++ src/csrc/kernel/count_nan.cu
@@ -1,8 +1,21 @@
 #include "count_nan.h"
 
+#include <cassert>
+
 #include "util/cuda_utils.h"
 
 namespace st::kernel {
+
+template<typename T>
+__device__ bool is_inf(T val) {
+	if constexpr (std::is_same<T, float>::value) {
+		return ((uint32_t*)(&val))[0] == 0x7f800000;
+	} else if constexpr (std::is_same<T, half>::value) {
+		return ((uint16_t*)(&val))[0] == 0x7c00;
+	} else {
+		assert(false);
+	}
+}
 
 template<typename T>
 __global__ void countNanKernel(
@@ -11,7 +24,7 @@ __global__ void countNanKernel(
 	int n
 ) {
 	for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x) {
-		if (arr[i] != arr[i]) {
+		if (arr[i] != arr[i] || is_inf<T>(arr[i]) || is_inf<T>(-arr[i])) {
 			atomicAdd(count, 1);
 		}
 	}
